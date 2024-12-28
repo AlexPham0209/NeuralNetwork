0b0ccf48840b3b9c402d9ea160fc7c6fc053bee1
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2024-12-28 13:47:55 -0500

    Fixed randomizer bug

--- network.py
+++ network.py
@@ -9,20 +9,24 @@ class Layer:
         self.randomize_weights()
         self.randomize_biases()
 
+        print(self.biases)
+        print(self.weights)
+        print()
+
     def feed_forward(self, a):
         output = [0] * self.neuron_size
 
-        for i, l in enumerate(zip(self.weights, self.biases)):
-            w, b = l
+        for i, n in enumerate(zip(self.weights, self.biases)):
+            w, b = n
             output[i] = np.dot(a, w) + b
 
         return output
         
     def randomize_weights(self):
-        self.weights = [[random.uniform(0.0, 1.0)] * self.weight_size for _ in range(self.neuron_size)]
+        self.weights = [[random.uniform(0.0, 1.0) for j in range(self.weight_size)] for i in range(self.neuron_size)]
 
     def randomize_biases(self):
-        self.biases = [random.uniform(0.0, 1.0)] * self.neuron_size
+        self.biases = [random.uniform(0.0, 1.0) for i in range(self.neuron_size)]
 
 
 class NeuralNetwork:
@@ -31,9 +35,10 @@ class NeuralNetwork:
         self.layers = [Layer(curr, prev) for curr, prev in zip(layer_size[1:], layer_size)]
 
     def feed_forward(self, a):
-        print(self.layers)
+        if len(a) != self.layer_size[0]:
+            return None
+        
         for layer in self.layers:
-            print(a)
             a = layer.feed_forward(a)
         
         return a
