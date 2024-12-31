e6e8c7c40f8ccaa0e0b600201044d6521625fbc1
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2024-12-31 00:27:07 -0500

    Added backpropagation

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import numpy as np
 import network as nw
 
 
-network = nw.NeuralNetwork([2, 2, 3, 2, 2])
+network = nw.NeuralNetwork([5, 2, 3, 2, 5])
 
-print(network.feed_forward([random.uniform(0, 1), random.uniform(0, 1)]))
+input = [0.2512, 0.12, 0.01, 0.54, 0.6]
+print(network.feed_forward(input))
+
+for i in range(1000):
+    network.backpropagation(input, [0, 1, 0, 0, 0])
+
+print(network.feed_forward(input))
