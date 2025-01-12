647f26ac5060117780066edcd027a0ff73d6eb0f
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2025-01-12 16:47:06 -0500

    Restructured files

--- programs/digitrecognition/digitrecognition.py
+++ programs/digitrecognition/digitrecognition.py
@@ -15,40 +15,38 @@ def read_digits(path, array = False):
     with open(path) as file:
         for data in file.read().strip().split("\n"):
             data = list(map(int, data.split(",")))
-            input = [val/255 for val in data[1:]]
 
             expected = data[0]
             if array:
-                expected = [0] * 10
+                expected = np.zeros(10)
                 expected[data[0]] = 1 
 
-            dataset.append((input, expected))
+            dataset.append((np.array(data[1:])/255, expected))
 
     return dataset
 
-train_data = read_digits("C:/Users/RedAP/Desktop/mnist_train.csv", True)
-test_data = read_digits("C:/Users/RedAP/Desktop/mnist_test.csv", False)
 
 def train_network():
-    network = nw.NeuralNetwork([ROW * COL, 50, 50, 10], path = "network.json")
-    network.learn(train_data, 5, 0.5, 10, debug=True)
+    train_data = read_digits("C:/Users/RedAP/Desktop/mnist_train.csv", True)
+    network = nw.NeuralNetwork([ROW * COL, 64, 64, 10])
+    network.learn(train_data, 10, 0.5, 10, debug=True)
 
     save_data = network.save_data()
-    with open("network.json", "w") as file:
+    with open("programs/digitrecognition/output/network.json", "w") as file:
         json.dump(save_data, file, indent = 3)
     
     test(network)
 
 def load_network():
-    network = nw.NeuralNetwork([ROW * COL, 30, 30, 10], path = "network.json")
+    network = nw.NeuralNetwork([ROW * COL, 64, 64, 10], path = "programs/digitrecognition/output/network.json")
     test(network)
 
 def test(network):
-    global test_data
+    test_data = read_digits("C:/Users/RedAP/Desktop/mnist_test.csv", False)
     correct = 0
     wrong = 0
     
-    output = open("output.txt", "w")
+    output = open("programs/digitrecognition/output/out.txt", "w")
     for data in test_data:
         input, expected = data
         actual, test = network.evaluate(input)
